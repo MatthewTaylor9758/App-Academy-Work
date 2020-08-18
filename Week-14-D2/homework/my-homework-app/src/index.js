0f61d7cd6bdb340adc8d08ff47a11520f8519e78
Matthew Taylor <matthewtaylor9758@gmail.com>
2020-08-18 18:14:21 -0400

    finish the second homework project

--- Week-14-D2/homework/my-homework-app/src/index.js
+++ Week-14-D2/homework/my-homework-app/src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import RandomQuote from './RandomQuote';
+import AlertButton from './AlertButton';
+import NoDefaultSubmitForm from './NoDefaultSubmitForm';
 
 ReactDOM.render(
   <React.StrictMode>
     <RandomQuote />
+    <AlertButton />
+    <NoDefaultSubmitForm />
   </React.StrictMode>,
   document.getElementById('root')
 );
