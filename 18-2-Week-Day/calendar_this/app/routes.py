d7848e3a32dcef007b6724c122024a2e5d695fd8
Matthew Taylor <matthewtaylor9758@gmail.com>
2020-09-16 11:13:15 -0400

    get list displayed on page

--- 18-2-Week-Day/calendar_this/app/routes.py
+++ 18-2-Week-Day/calendar_this/app/routes.py
@@ -1,7 +1,23 @@
 from flask import  (Flask, Blueprint, render_template)
+import os
+import psycopg2
+from psycopg2.extras import DictCursor
 
 bp = Blueprint('main', __name__, "/")
 
+CONNECTION_PARAMETERS = {
+    "user": os.environ.get("DB_USER"),
+    "password": os.environ.get("DB_PASS"),
+    "dbname": os.environ.get("DB_NAME"),
+    "host": os.environ.get("DB_HOST"),
+}
+
 @bp.route('/')
 def main():
-  return render_template('main.html')
+  with psycopg2.connect(**CONNECTION_PARAMETERS, cursor_factory=DictCursor) as conn:
+    with conn.cursor() as curs:
+      curs.execute('''SELECT id, name, start_datetime, end_datetime
+                      FROM appointments
+                      ORDER BY start_datetime''')
+      while rows := curs.fetchall():
+        return render_template('main.html', rows=rows)
